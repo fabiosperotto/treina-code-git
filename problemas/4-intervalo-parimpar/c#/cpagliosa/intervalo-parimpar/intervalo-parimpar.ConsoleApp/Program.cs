4afe7634d47dbcd5d1ffdb220aacc1a0a7f620fe
Caio Pagliosa <caio.pinipagli@gmail.com>
2024-04-26 11:21:36 -0300

    problema 4 resolvido

---
+++ problemas/4-intervalo-parimpar/c#/cpagliosa/intervalo-parimpar/intervalo-parimpar.ConsoleApp/Program.cs
@@ -0,0 +1,49 @@
+﻿
+    internal class Program
+{
+    static LinkedList<int> lista = new LinkedList<int>();
+    static int par = 0;
+    static int impar = 0;
+    static void Main(string[] args)
+    {
+        Console.Write("Informe o valor base para o intervalo: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Informe o valor limite o intervalo: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        while (true)
+        {
+            Console.Write("Informe um numero qualquer: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            if (x >= a && x <= b)
+            {
+                lista.AddFirst(x);
+            }
+
+            if (x == 0) break;
+        }
+        Contador();
+
+        Console.Write($"\nPara o intervalo {a} e <= {b} existem {par} numeros pares  e {impar} numeros impares.\n");
+        Console.Write("\nenter para finalizar...");
+        Console.ReadLine();
+    }
+
+    static void Contador()
+    {
+        foreach (int x in lista)
+        {
+            if ((x % 2) == 0)
+            {
+                par++;
+            }
+            else
+            {
+                impar++;
+            }
+        }
+    }
+}
+
