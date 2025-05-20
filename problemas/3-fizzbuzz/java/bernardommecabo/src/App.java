15d7e16d7a9772707b83379908dc60506bd8fd67
bernardomecabo <bernardomecabo09@gmail.com>
2025-05-20 16:30:11 -0300

    Fix - Resolvendo o desafio 3 - FizzBuzz em Java

--- problemas/3-fizzbuzz/java/bernardommecabo/src/App.java
+++ problemas/3-fizzbuzz/java/bernardommecabo/src/App.java
@@ -1,5 +1,8 @@
 import fizzbuzz.FizzBuzz; 
 
+/**
+ * @author bernardommecabo
+ */
 public class App {
     public static void main(String[] args) throws Exception {
         // Cria uma instância da classe FizzBuzz e chama o método output para obter o resultado
